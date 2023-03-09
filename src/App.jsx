d051f8d096257a10d4ce13d24f7ec5c002b2213f
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-03-09 00:43:52 +0000

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>   
+        <Post 
+        author="Thiago Cezario" 
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, inventore iusto velit quod laboriosam aliquam exercitationem. Sunt consectetur, corrupti velit voluptatibus ex maxime repudiandae quidem molestiae voluptate earum asperiores at?"
+      />
+
+      <Post 
+      author="Joãozinho carmo"
+      content="a é isso ai nao sabemos mais. ook?"
+      
+      />
+   </>
+
+  )
+}
+
